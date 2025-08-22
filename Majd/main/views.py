b404c91292e36222ddbc2bfd68ed751b2073ae90
AbdullahSofayan <444101428@student.ksu.edu.sa>
2025-08-22 22:09:25 +0300

    create forms for contact page + style it

--- Majd/main/views.py
+++ Majd/main/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import ContactForm
 
 def main_home_view(request):
     return render(request, "main/main_home.html")
 
 
 def contact_view(request):
-    
-    return render(request, "main/contact_us.html")
+    form = ContactForm(request.POST)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+
+    return render(request, "main/contact_us.html" ,{"form": form})
 
 
